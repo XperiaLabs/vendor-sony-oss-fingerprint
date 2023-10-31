eb8112f4c3f3f32e865c304de57ec00a62c953c6
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2023-10-31 19:41:28 +0000

    adapt makefile

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(PRODUCT_PLATFORM_SOD),true)
-ifneq ($(TARGET_DEVICE_NO_FPC), true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -12,74 +10,10 @@ LOCAL_SRC_FILES := \
     QSEEComFunc.c \
     common.c
 
-# ---------------- FPC ----------------
-ifeq ($(filter-out loire tone,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_loire_tone.c
-HAS_FPC := true
-endif
-
-ifeq ($(filter-out yoshino,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_YOSHINO
-endif
-
-ifeq ($(filter-out nile,$(SOMC_PLATFORM)),)
-# NOTE: Nile can have either FPC or Egistec
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_NILE \
-    -DHAS_LEGACY_EGISTEC
-endif
-
-ifeq ($(filter-out tama,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_TAMA
-endif
-
-# ---------------- Egistec ----------------
-ifneq ($(filter-out loire tone yoshino tama,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += -DFINGERPRINT_TYPE_EGISTEC
-endif
-
-ifeq ($(filter-out kumano seine edo sagami,$(SOMC_PLATFORM)),)
 LOCAL_CFLAGS += \
     -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
     -DEGIS_QSEE_APP_NAME=\"egista\" \
-    -DEGIS_QSEE_APP_PATH=\"/odm/firmware\"
-else ifeq ($(filter-out lena,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
-    -DEGIS_QSEE_APP_NAME=\"egista\"
-else ifeq ($(filter-out nagara,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
-    -DEGIS_QSEE_APP_PATH=\"/odm/firmware\" \
-    -DEGIS_QSEE_APP_NAME=\"egista64\"
-else ifeq ($(filter-out murray zambezi,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
-    -DEGIS_QSEE_APP_NAME=\"egisap\"
-else
-LOCAL_CFLAGS += \
-    -DEGIS_QSEE_APP_NAME=\"egisap32\"
-endif
-
-# Define dynamic power management for everything but the following platforms:
-ifneq ($(filter-out kumano seine,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifneq ($(HAS_FPC),true)
-# This file heavily depends on fpc_ implementations from the
-# above fpc_imp_* files. There is no sensible default file
-# on some platforms, so just remove the file altogether:
-LOCAL_SRC_FILES -= BiometricsFingerprint.cpp
-endif
+    -DHAS_DYNAMIC_POWER_MANAGEMENT
 
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.biometrics.fingerprint@2.1 \
@@ -90,11 +24,6 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     liblog \
     libutils
-
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-endif
 
 LOCAL_CFLAGS += \
     -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) \
